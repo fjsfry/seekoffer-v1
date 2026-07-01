2551efbeb6ddcb98f75cad81efeb8f9d07cfcb18
JiaSheng Fu <1352256858@qq.com>
2026-07-02 00:54:21 +0800

    Serve Baidu verification route

---
+++ app/baidu_verify_codeva-x5pn9knuby.html/route.ts
@@ -0,0 +1,9 @@
+export const dynamic = 'force-static';
+
+export function GET() {
+  return new Response('codeva-x5pn9knuby', {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8'
+    }
+  });
+}
